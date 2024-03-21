0b66f43b03ef5f52ee519266ef94cc2dc55cb822
Ian Tozer <23465548+itzr@users.noreply.github.com>
2024-03-21 16:24:11 +0700

    x

--- src/components/AOLuaMandelbrotSet.js
+++ src/components/AOLuaMandelbrotSet.js
@@ -19,6 +19,7 @@ const MandelbrotSet = () => {
   const handleConnectWallet = async () => {
     try {
       const arweaveWallet = new ArweaveWebWallet();
+      arweaveWallet.setUrl("arweave.app");
 
       await arweaveWallet.connect();
 
